abea22e3b8bfaeb5376732641d5f6c9a946eeef1
Ask1519 <alivalizade@gmail.com>
2022-06-14 18:24:23 +0430

    super mario new update

--- SuperMario/Assets/Scripts/Mario.cs
+++ SuperMario/Assets/Scripts/Mario.cs
@@ -7,13 +7,15 @@ public class Mario : MonoBehaviour
     public float speed = 20;
     public float force = 5;
     public Rigidbody2D player;
+    public int score;
+    public GameObject Enemy;
+
     // Start is called before the first frame update
     void Start()
     {
         player = GetComponent<Rigidbody2D>();
         
     }
-
     // Update is called once per frame
     void Update()
     {
@@ -21,8 +23,14 @@ public class Mario : MonoBehaviour
         player.velocity = new Vector2(x*speed,player.velocity.y);
 
         if(Input.GetKeyDown(KeyCode.UpArrow))
-       player.velocity += Vector2.up*force;
-    
-        
+       player.velocity += Vector2.up*force;    
     }
+    void OnCollisionEnter2D(Collision2D Enemy)
+    {
+        if(Enemy.gameObject.name.StartsWith("Enemy"))
+        {
+           Destroy(Enemy.gameObject);
+        }
+    }
+    
 }
